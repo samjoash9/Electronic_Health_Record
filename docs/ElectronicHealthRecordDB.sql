d84239474d2dfd0e88b051152e86714b02def612
joashantonio <sam.antonio@sindbad.tech>
2026-08-25 13:26:35 +0800

    feat: add SQL schema for patient management and update .gitignore

---
+++ docs/ElectronicHealthRecordDB.sql
@@ -0,0 +1,170 @@
+/*
+CREATE TABLE Patient (
+	PatientID INT PRIMARY KEY IDENTITY(1, 1),
+	Surname NVARCHAR(50) NOT NULL,
+	FirstName NVARCHAR(50) NOT NULL,
+	MiddleName NVARCHAR(50) NULL,
+	Birthdate DATE NOT NULL,
+	Sex CHAR(1) NOT NULL,
+	CivilStatus VARCHAR(20) NOT NULL,
+	Address NVARCHAR(255) NULL,
+	AgencyOffice NVARCHAR(100) NULL,
+	Position NVARCHAR(50) NULL,
+	ContactNo VARCHAR(20) NULL,
+    CreatedAt DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
+    UpdatedAt DATETIME2 NOT NULL DEFAULT SYSDATETIME()
+);
+
+CREATE TABLE Physician (
+	PhysicianID INT PRIMARY KEY IDENTITY(1, 1),
+	Surname NVARCHAR(50) NOT NULL,
+	FirstName NVARCHAR(50) NOT NULL,
+	MiddleName NVARCHAR(50) NULL,
+	PRCLicenseNo NVARCHAR(20) NOT NULL UNIQUE,
+	CreatedAt DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
+    UpdatedAt DATETIME2 NOT NULL DEFAULT SYSDATETIME()
+);
+
+CREATE TABLE Admin (
+    AdminID INT PRIMARY KEY IDENTITY(1, 1),
+    Username NVARCHAR(30) NOT NULL UNIQUE,
+    Email NVARCHAR(255) NOT NULL UNIQUE,
+    PasswordHash NVARCHAR(255) NOT NULL,
+    FullName NVARCHAR(100) NOT NULL,
+    IsActive BIT NOT NULL DEFAULT 1,
+    LastLoginAt DATETIME2 NULL,
+    CreatedAt DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
+    UpdatedAt DATETIME2 NOT NULL DEFAULT SYSDATETIME()
+);
+
+CREATE TABLE AdminSession (
+    SessionID INT PRIMARY KEY IDENTITY(1, 1),
+    AdminID INT NOT NULL,
+    TokenHash CHAR(64) NOT NULL UNIQUE,
+    ExpiresAt DATETIME2 NOT NULL,
+    RevokedAt DATETIME2 NULL,
+    CreatedAt DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
+
+    CONSTRAINT FK_AdminSession_Admin
+        FOREIGN KEY (AdminID)
+			REFERENCES Admin(AdminID)
+);
+
+CREATE TABLE WellnessForm (
+	FormID INT PRIMARY KEY IDENTITY(1, 1),
+	PatientID INT NOT NULL,
+	PhysicianID INT NOT NULL,
+	FormDate DATE NOT NULL DEFAULT CAST(SYSDATETIME() AS DATE),
+	-- DECIMAL -> 5 digits, 2 floating points (999.99kg max)
+	WeightKg DECIMAL(5, 2) NULL,
+	HeightCm DECIMAL(5, 2) NULL,
+	BMI DECIMAL(5, 2) NULL,
+	IdealBMI DECIMAL(5, 2) NULL,
+	BPSystolic SMALLINT NULL,
+	BPDiastolic SMALLINT NULL,
+	-- 3 igits, 1 floating point (36.5, 99.9)
+	TempCelsius DECIMAL(3, 1) NULL,
+	HeartRate SMALLINT NULL,
+	RespRate SMALLINT NULL,
+	RecommendedDiagnosticTest NVARCHAR(150) NULL,
+	ImpressionClinical NVARCHAR(300) NULL,
+	ManagementTreatment NVARCHAR(300) NULL,
+
+	CreatedByAdminID INT NULL,
+	UpdatedByAdminID INT NULL,
+
+	CreatedAt DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
+    UpdatedAt DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
+
+	-- FOREIGN KEYS (PatientID & PhysicianID)
+	CONSTRAINT FK_WellnessForm_Patient
+		FOREIGN KEY (PatientID)
+			REFERENCES Patient(PatientID),
+
+	CONSTRAINT FK_WellnessForm_Physician
+		FOREIGN KEY (PhysicianID)
+			REFERENCES Physician(PhysicianID),
+
+	CONSTRAINT FK_WellnessForm_CreatedByAdmin
+		FOREIGN KEY (CreatedByAdminID)
+			REFERENCES Admin(AdminID)
+			ON DELETE SET NULL,
+
+	CONSTRAINT FK_WellnessForm_UpdatedByAdmin
+		FOREIGN KEY (UpdatedByAdminID)
+			REFERENCES Admin(AdminID)
+			ON DELETE NO ACTION
+);
+
+CREATE TABLE MedicalCondition (
+	ConditionID INT PRIMARY KEY IDENTITY(1, 1),
+	ConditionName NVARCHAR(50) UNIQUE NOT NULL,
+	ConditionType NVARCHAR(100) NULL
+);
+
+CREATE TABLE SocialHistory (
+	SocialHistoryID INT PRIMARY KEY IDENTITY(1, 1),
+	FormID INT NOT NULL UNIQUE,
+	SmokingSticksPerDay SMALLINT NULL,
+	AlcoholType NVARCHAR(50) NULL,
+	DrinkFrequency NVARCHAR(50) NULL,
+	DrinksPerSession NVARCHAR(20) NULL,
+	HasBeenDrunk bit NULL,
+	DrunkFrequency NVARCHAR(50) NULL,
+	ExerciseFrequency NVARCHAR(50) NULL,
+	-- may contain many exercise types
+	ExerciseType NVARCHAR(100) NULL,
+
+	CreatedAt DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
+    UpdatedAt DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
+
+	CONSTRAINT FK_SocialHistory_WellnessForm
+		FOREIGN KEY (FormID)
+			REFERENCES WellnessForm(FormID)
+);
+
+CREATE TABLE FamilyMedicalHistory (
+	FMHID INT PRIMARY KEY IDENTITY(1, 1),
+	FormID INT NOT NULL,
+	ConditionID INT NULL,
+	ConditionOther NVARCHAR(100) NULL,
+	FamilyMember NVARCHAR(50) NULL,
+	IsNone bit NULL DEFAULT 0,
+
+	CreatedAt DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
+	UpdatedAt DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
+
+	CONSTRAINT FK_FMH_WellnessForm	
+		FOREIGN KEY (FormID)
+			REFERENCES WellnessForm(FormId),
+	CONSTRAINT FK_FMH_Condition
+		FOREIGN KEY (ConditionID)
+			REFERENCES MedicalCondition (ConditionID)
+);
+
+CREATE TABLE PastMedicalHistory (
+	PMHID INT PRIMARY KEY IDENTITY(1, 1),
+	FormID INT NOT NULL,
+	ConditionID INT NULL,
+
+	ConditionOther NVARCHAR(100) NULL,
+	YearDiagnosed SMALLINT NULL,
+	MaintenanceDrugGeneric NVARCHAR(100) NULL,
+	Dosage NVARCHAR(20) NULL,
+	Frequency NVARCHAR(50) NULL,
+
+	CreatedAt DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
+	UpdatedAt DATETIME2 NOT NULL DEFAULT SYSDATETIME(),
+
+	CONSTRAINT FK_PMH_WellnessForm
+		FOREIGN KEY (FormID)
+			REFERENCES WellnessForm (FormID),
+	CONSTRAINT FK_PMH_Condition
+		FOREIGN KEY (ConditionID)
+			REFERENCES MedicalCondition (ConditionID)
+);
+
+*/
+
+
+SELECT * FROM Patient;
