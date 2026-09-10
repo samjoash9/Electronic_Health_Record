d75b4a3659230ed3e3e939cbc410e782546bbe9e
rodssss <jhonrods2004@gmail.com>
2026-09-10 13:13:01 +0800

    Add Lenght in Employee Model

---
+++ Electronic_Health_Record.Server/Data/Migrations/20260910051206_AddLenghtEmployeeModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Electronic_Health_Record.Server.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLenghtEmployeeModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Electronic_Health_Record.Server/Models/Employee.cs
+++ Electronic_Health_Record.Server/Models/Employee.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 namespace Electronic_Health_Record.Server.Models
 {
     // Local stand-in for the external HR API. Station 1 searches this table today;
@@ -7,18 +8,25 @@ namespace Electronic_Health_Record.Server.Models
     {
         public int EmployeeID { get; set; }
         public string ExternalEmployeeId { get; set; } = string.Empty;
+        [MaxLength(150)]
         public string Surname { get; set; } = string.Empty;
+        [MaxLength(150)]
         public string FirstName { get; set; } = string.Empty;
         public string? MiddleName { get; set; }
         public DateTime Birthdate { get; set; }
         public int Age { get; set; }
+        [MaxLength(150)]
         public string Sex { get; set; } = string.Empty;
+        [MaxLength(150)]
         public string CivilStatus { get; set; } = string.Empty;
         public string? Address { get; set; }
-        
+        [MaxLength(200)]
         public string? AgencyOffice { get; set; }
 
+
+        [MaxLength(400)]
         public string Position { get; set; }
+        [MaxLength(150)]
         public string? ContactNo { get; set; }
     }
 }
